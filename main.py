a358d5b203f295f9e1455c5d0c3c548da2e1b58e
Alejandro <alejandroecf01@gmail.com>
2023-06-19 00:08:45 -0500

    Working alarm

---
+++ main.py
@@ -0,0 +1,9 @@
+from os import startfile
+import random
+
+files = [r"C:\Users\alejandro\PycharmProjects\pythonProject\Joey Diaz BEST MOTIVATION EVER 🤯 joeydiaz motivation shorts.mp4",
+         r"C:\Users\alejandro\PycharmProjects\pythonProject\YOUR NEW MORNING ALARM.mp4",
+         r"C:\Users\alejandro\JoeyNationalAnthem.mp4",
+         r"C:\Users\alejandro\GETUP.mp3"]
+
+startfile(random.choice(files))
